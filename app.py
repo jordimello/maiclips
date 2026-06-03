9a5d223edeb66c9aba20e1df94f91c4677e3678e
MaiClips <jordimelvin30@gmail.com>
2026-06-03 04:23:50 +0200

    fix port

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def upload():
     return jsonify({"clips": clips, "transcript": result['text']})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+      app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
